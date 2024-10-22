25922cb68f2091cbfcb5ec827ffd0dc5bc1b8652
Ajoysingh9t <146538937+Ajoysingh9t@users.noreply.github.com>
2024-10-22 02:44:39 +0000

    add snippets and input/output file

--- c++_programming/2.cpp
+++ c++_programming/2.cpp
@@ -1,5 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define ll long long
+#define yes "YES\n"
+#define no "NO\n"
 using namespace std;
-int main(){
-    cout<<"hello world\n";
+void inputOutput()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+void solve()
+{
+    string s,t;
+    cin>>s>>t;
+    map<char,int>fre_s;
+    map<char,int>fre_t;
+    for(char c:s){
+        fre_s[c]++;
+    }
+    for(char c:t){
+        fre_t[c]++;
+    }
+    for(auto it:fre_s){
+        cout<<it.first<<" -> "<<it.second<<endl;
+    }
+}
+
+int main()
+{
+    inputOutput();
+    solve();
+    return 0;
 }
